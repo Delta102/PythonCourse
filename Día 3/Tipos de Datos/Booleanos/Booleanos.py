da3a12f65861f1f7eaf4a2bce0ea5aab38a361a9
Delta102 <alfrefp10001@gmail.com>
2022-12-17 23:15:43 -0500

    Actualización Booleanos

---
+++ Día 3/Tipos de Datos/Booleanos/Booleanos.py
@@ -0,0 +1,22 @@
+## PARTE 1
+    #En python los valores de booleano inician con mayúscula
+var1 = True
+var2 = False
+
+print(type(var1))
+
+## PARTE 2
+
+numero = 5 >= 2+3    ## Condicional
+print(type(numero))
+print(numero)
+
+
+numero = bool() ## FALSE: Valor Default
+print(numero)
+
+## PARTE 3
+
+lista = [1,2,3,4]
+control = 5 in lista ##Si 5 se encuentra en mi lista
+print(control)
---
+++ Día 3/Tipos de Datos/Booleanos/Ejercicios.py
@@ -0,0 +1,19 @@
+##1
+    # Realiza una comparación que arroje como resultado un booleano y almacena el resultado (True/False) en una variable llamada prueba
+
+prueba = 5 > 3
+
+## 2
+    # Verifica si 17834/34 es mayor que 87*56 y muestra el resultado (booleano) en pantalla utilizando print()
+
+if((17834/34)>(87*56)):
+    print(True)
+
+else:
+    print(False)
+
+## 3
+    # Verifica si la raíz cuadrada de 25 es igual a 5 y muestra el resultado (booleano) en pantalla utilizando print()
+
+resultadoBool = 5 == 5
+print(resultadoBool)
