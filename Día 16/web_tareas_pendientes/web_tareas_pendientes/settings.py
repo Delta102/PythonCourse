c266884e3960c2c27dc9b930037e8615715a1960
Felix Peralta Rodríguez <109428571+Delta102@users.noreply.github.com>
2023-01-29 10:57:37 -0500

    Update settings.py

--- Día 16/web_tareas_pendientes/web_tareas_pendientes/settings.py
+++ Día 16/web_tareas_pendientes/web_tareas_pendientes/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ujp1!a+2=yl_v^p#rc7)tybnskofj8n93c4n#g*li%mb#f-6&a'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
